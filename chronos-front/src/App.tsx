d3b8e670d472b98ae5e29ee4d7d8e33a4adc6b6f
nechamdod <nechamdod@gmail.com>
2024-03-09 15:15:39 +0200

    init project

---
+++ chronos-front/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <a href="https://www.notion.so/Chronos-Teamspace-0d4d2fcee0f8423591a8ea32292d5138">Follow the link</a>
+    </>
+  )
+}
+
+export default App
