d14303de882d6131ddab1781153cc6a6bc1a2a9d
nechamdod <nechamdod@gmail.com>
2024-03-13 14:30:46 +0200

    feat: add new routes

--- chronos-front/src/Routes/Routes.tsx
+++ chronos-front/src/Routes/Routes.tsx
@@ -5,6 +5,8 @@ import {SignUpPage} from "../pages/SignUpPage/SingUpPage";
 import App from "../App.tsx";
 import {ErrorPage} from "../pages/NotFoundPage/NotFoundPage.tsx";
 import {SignInPage} from "../pages/SignInPage/SignInPage.tsx";
+import {VerifyPage} from "../pages/VerifyPage/VerifyPage.tsx";
+import {HomePage} from "../pages/HomePage/HomePage.tsx";
 
 export const router = createBrowserRouter([
     {
@@ -20,7 +22,16 @@ export const router = createBrowserRouter([
     {
         path: "signin",
         element: <SignInPage/>
+    },
+    {
+        path: "verify",
+        element: <VerifyPage/>
+    },
+    {
+        path: "calendar",
+        element: <HomePage/>
     }
+
 
 
 ]);
