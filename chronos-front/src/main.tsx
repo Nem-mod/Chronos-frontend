1b64642e5af2f87ba83463875e5d50774ac892d3
nechamdod <nechamdod@gmail.com>
2024-03-15 19:01:21 +0200

    quick fixes

--- chronos-front/src/main.tsx
+++ chronos-front/src/main.tsx
@@ -1,20 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {RouterProvider} from "react-router-dom"
-import {Provider} from "react-redux";
-import {store} from "./store/store.ts"
-import {router} from "./Routes/Routes.tsx";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
+import { router } from './Routes/Routes.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-            <RouterProvider router={router}/>
-        </DevSupport>
+        <RouterProvider router={router} />
     </Provider>,
-)
+);
 
