317c9167d0be803289c6608bdd6b3807d20c2227
nechamdod <nechamdod@gmail.com>
2024-03-15 18:52:50 +0200

    fix

--- chronos-front/src/App.tsx
+++ chronos-front/src/App.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 function App() {
     return (
         <div>
-            <Link className={"text-blue-600 block"} to={"/signing"}>Sign in</Link>
+            <Link className={"text-blue-600 block"} to={"/signin"}>Sign in</Link>
             <Link className={"text-blue-600 block"} to={"/signup"}>Sign up</Link>
 
         </div>
