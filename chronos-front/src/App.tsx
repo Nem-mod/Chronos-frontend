39c60f8ee6d1af9ed9f863cd513fd5b83984adb7
nechamdod <nechamdod@gmail.com>
2024-03-19 19:30:11 +0200

    feat: hmm

--- chronos-front/src/App.tsx
+++ chronos-front/src/App.tsx
@@ -1,37 +1,11 @@
 import './App.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAppDispatch } from './hooks/redux-hooks.ts';
-import { useEffect } from 'react';
-import { fetchAuthMe } from './store/slices/auth.ts';
+import { Link } from 'react-router-dom';
 
 function App() {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const auth = async () => {
-            const { error } = await dispatch(
-                fetchAuthMe(null),
-            );
-
-            if (error) {
-                throw error;
-            }
-
-            navigate('/calendar');
-        };
-
-
-        auth().catch(() => {
-            navigate('/signin');
-        });
-
-    }, []);
     return (
         <div>
             <Link className={'text-blue-600 block'} to={'/signin'}>Sign in</Link>
             <Link className={'text-blue-600 block'} to={'/signup'}>Sign up</Link>
-
         </div>
 
     );
