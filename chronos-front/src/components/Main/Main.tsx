6221106034a546860313d0b1c64f5cc55d276167
nechamdod <nechamdod@gmail.com>
2024-03-24 15:28:24 +0200

    feat: add calendar to main page

--- chronos-front/src/components/Main/Main.tsx
+++ chronos-front/src/components/Main/Main.tsx
@@ -1,7 +1,9 @@
+import { CalendarTest } from '../CalendarTest/CalendarTest.tsx';
+
 export const Main = () => {
     return (
         <main className={'flex-grow pr-6 pl-6'}>
-
+            <CalendarTest />
         </main>
     );
 };
