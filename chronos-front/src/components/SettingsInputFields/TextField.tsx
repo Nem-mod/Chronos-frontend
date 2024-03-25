949539a6dbcdcab076dbe78a3a8d047a390ffa12
nechamdod <nechamdod@gmail.com>
2024-03-25 16:11:50 +0200

    feat: update textfield

--- chronos-front/src/components/SettingsInputFields/TextField.tsx
+++ chronos-front/src/components/SettingsInputFields/TextField.tsx
@@ -1,21 +1,30 @@
 interface Props {
-    label: string
-    value: string,
-    onChange: (value: string) => void
+    label: string;
+    value: string;
+    onChange: (value: string) => void;
+    className?: string;
 }
 
-export const TextField = ({ label, value, onChange }: Props) => {
+export const TextField = ({ label, value, onChange, className }: Props) => {
     return (
         <div className={'mt-2 mb-2'}>
-            <label>
-                <p className={'pt-2 pb-2'}>
-                    {label}
-                </p>
+            {label ? (
+                    <label>
+                        <p className={'pt-2 pb-2'}>
+                            {label}
+                        </p>
+                        <input type='text' placeholder='text'
+                               onChange={(e) => onChange(e.target.value)}
+                               value={value ? value : ''}
+                               className={className || `input input-bordered w-full max-w-xs`} />
+                    </label>
+
+                ) :
                 <input type='text' placeholder='text'
                        onChange={(e) => onChange(e.target.value)}
                        value={value ? value : ''}
-                       className='input input-bordered w-full max-w-xs' />
-            </label>
+                       className={className || `input input-bordered w-full max-w-xs`} />
+            }
         </div>
     );
 };
